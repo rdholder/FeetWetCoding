ecb435e2af2328ed4e441a53b07c8d7db8562590
robert <robert@feetwetcoding.com>
2012-02-12 10:15:49 -0700

    Added comment in 400_C01S05_OYO5.cpp
    Signed-off-by: robert <robert@feetwetcoding.com>

--- exercises/C01-Beginner_Exercises/S05-Loops_Logic/400_C01S05_OYO5.cpp
+++ exercises/C01-Beginner_Exercises/S05-Loops_Logic/400_C01S05_OYO5.cpp
@@ -12,5 +12,7 @@
 
 void C01S05_OYO5::runExercise()
 {
-
+    // rdh11feb2012: note to the user, i'm still working on this exercise.
+    // it's not really doing exactly what i want at this point, so if you
+    // are having trouble with it, just move on, and come back to it later.  :-)
 }
