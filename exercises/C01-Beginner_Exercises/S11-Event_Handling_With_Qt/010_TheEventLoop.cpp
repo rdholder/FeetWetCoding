6fdd458fe16a8f60b01ecc44ad6f4f28c6aee95f
janice <janice@feetwetcoding.com>
2012-02-26 12:22:28 -0700

    Started exercise TheEventLoop. Nothing here yet, new code is experimental and commented out for now.
    Signed-off-by: janice <janice@feetwetcoding.com>

--- exercises/C01-Beginner_Exercises/S11-Event_Handling_With_Qt/010_TheEventLoop.cpp
+++ exercises/C01-Beginner_Exercises/S11-Event_Handling_With_Qt/010_TheEventLoop.cpp
@@ -8,3 +8,59 @@ void TheEventLoop::runExercise()
 {
     DrawText("Exercise Under\nConstruction", 50, 100, BLUE, 24);
 }
+
+
+// START OF POTENTIAL EXERCISE CODE
+// NOT SURE THIS IS WHAT WE WANT BUT
+// DON'T WANT TO LOSE IT JUST YET
+// --jdugger
+//
+//
+//#include <QApplication>
+//#include <QPushButton>
+//using std::string;
+
+////This is the drawing area where the exercises
+////get rendered. It comes from feetwetcodinglib.h
+//extern FWCView *view;
+
+//class ExerciseWidget : public QFrame
+//{
+//public:
+//    ExerciseWidget() : QFrame(NULL){}
+//    ~ExerciseWidget(){}
+
+//    void setBackgroundColor( Color background=LIGHTGRAY );
+
+//protected slots:
+//    virtual void mousePressEvent ( QMouseEvent * event );
+
+//};
+
+//void ExerciseWidget::setBackgroundColor( Color background )
+//{
+//    DrawRectangle(0, 0, this->width(), this->height(), background, 1, true);
+//}
+
+//void ExerciseWidget::mousePressEvent ( QMouseEvent * event )
+//{
+//    Gsoln = false;
+
+//    this->setBackgroundColor();
+//    int x = event->x();
+//    int y = event->y();
+
+//    //TODO: GIVE THEM std::ostringstream AND std::string LESSON
+//    std::ostringstream message;
+//    message << "Clicked: " << x << ", " << y;
+
+//    DrawText("X", x-13, y-25, RED, 24);
+//    DrawText(message.str(), x+15, y-15, BLUE);
+//}
+
+//void TheEventLoop::runExercise()
+//{
+//    ExerciseWidget* widget = new ExerciseWidget();
+//    widget->setGeometry(0, 0, 400, 400);
+//    view->scene()->addWidget(widget);
+//}
